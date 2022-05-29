7dd9ad76bf2d2e6b41d45645bc5e070be33543d1
beckerinj <becker@assc.ai>
2022-05-29 21:18:17 +0300

    new monitor cli version and restart periphery command

--- restartPeripheryDefaults.sh
+++ restartPeripheryDefaults.sh
@@ -2,4 +2,4 @@ docker pull mbecker2020/monitor-periphery
 
 docker stop monitor-periphery && docker container rm monitor-periphery
 
-docker run -d --name monitor-periphery -v /home/ubuntu/secrets:/secrets -v /var/run/docker.sock:/var/run/docker.sock -v /home/ubuntu/monitor/:/monitor-root -v /home/ubuntu/.pm2:/.pm2 -p 8000:8000 -e SYSROOT=/home/ubuntu/monitor/ --restart no mbecker2020/monitor-periphery
+docker run -d --name monitor-periphery -v /home/ubuntu/secrets:/secrets -v /var/run/docker.sock:/var/run/docker.sock -v /home/ubuntu/monitor/:/monitor-root -p 8000:8000 -e SYSROOT=/home/ubuntu/monitor/ --restart unless-stopped --add-host=host.docker.internal:host-gateway mbecker2020/monitor-periphery
