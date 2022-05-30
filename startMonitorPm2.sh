a70718fa868b67ead3e718c2dfe400c7cea43bd7
beckerinj <becker@assc.ai>
2022-05-30 13:12:33 +0300

    improve pm2 client startup script

--- startMonitorPm2.sh
+++ startMonitorPm2.sh
@@ -1,5 +1,11 @@
 echo "starting monitor pm2 client"
 
+cd ~
+
+pm2 delete monitor-pm2
+
+sudo rm -r monitor-pm2
+
 git clone https://github.com/mbecker20/monitor-pm2.git
 
 cd monitor-pm2 && npm i && npm run start
