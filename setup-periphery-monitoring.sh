9d5b9fdf897ba0d0058f9fe9d7e95c7509fb2849
beckerinj <becker@anticapital.ai>
2023-01-11 14:42:40 -0500

    try this one

--- setup-periphery-monitoring.sh
+++ setup-periphery-monitoring.sh
@@ -10,7 +10,18 @@ monitor periphery gen-config
 
 cargo install monitor_periphery
 
-UNIT_FILE="[Unit]%s\nDescription=agent to connect with monitor core%s\nAfter=network.target%s\n%s\n[Service]%s\nType=simple%s\nUser=$USER%s\nWorkingDirectory=$HOME%s\nExecStart=/bin/bash --login -c 'source $HOME/.bashrc; $HOME/.cargo/bin/periphery --config-path ~/.monitor/periphery.config.toml --home-dir $HOME'%s\nTimeoutStartSec=0%s\n%s\n[Install]%s\nWantedBy=default.target"
+UNIT_FILE="[Unit]
+Description=agent to connect with monitor core
+After=network.target
+[Service]
+Type=simple
+User=$USER
+WorkingDirectory=$HOME
+ExecStart=/bin/bash --login -c 'source $HOME/.bashrc; $HOME/.cargo/bin/periphery --config-path ~/.monitor/periphery.config.toml --home-dir $HOME'
+TimeoutStartSec=0
+
+[Install]
+WantedBy=default.target"
 
 sudo printf $UNIT_FILE > /etc/systemd/system/periphery.service
 
